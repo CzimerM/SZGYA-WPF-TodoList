6676b40ea66b18410c8dd0292da17364573d4fe3
Csizmadia Márk <airamek@teamorange.hu>
2024-04-09 13:15:36 +0200

    Add project files.

---
+++ SZGYA-WPF-TodoList/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SZGYA_WPF_TodoList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //foreach (ListBoxItem x in lstTodoBox.Items)
+            //{
+            //    MessageBox.Show(x.Content.ToString());
+            //}
+            lstTodoBox.Items.Add(new TodoItem() { Title = "test" });
+        }
+    }
+
+    public class TodoItem
+    {
+        public string Title { get; set; }
+    }
+}
